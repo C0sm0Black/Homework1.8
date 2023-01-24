fcd002edc53026c09698754875b1667d95bdc509
CosmoBlack <soulker_mozyr@yahoo.com>
2023-01-24 11:38:40 +0200

    Task 2

--- src/Homework.java
+++ src/Homework.java
@@ -3,6 +3,41 @@ public class Homework {
     public static void main(String[] args) {
 
         task1();
+        task2();
+
+    }
+
+    private static void task2() {
+
+        System.out.println("Задача 2");
+
+        int[] array1 = new int[]{1, 2, 3};
+        double[] array2 = {1.57, 7.654, 9.986};
+        String[] array3 = {"f", "d", "c"};
+
+        for (int i = 0; i < array1.length - 1; i++) {
+
+            System.out.print(array1[i] + ", ");
+
+        }
+
+        System.out.println(array1[array1.length - 1]);
+
+        for (int i = 0; i < array2.length - 1; i++) {
+
+            System.out.print(array2[i] + ", ");
+
+        }
+
+        System.out.println(array2[array2.length - 1]);
+
+        for (int i = 0; i < array3.length - 1; i++) {
+
+            System.out.print(array3[i] + ", ");
+
+        }
+
+        System.out.println(array3[array3.length - 1]);
 
     }
 
